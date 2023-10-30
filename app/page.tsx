ccb2dc6bd0d98e79d00f017e365614a4ab060bfa
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-10-30 19:15:47 -0400

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/main/hero'
+import Skills from '@/components/main/skills'
 import Image from 'next/image'
 
 export default function Home() {
   return (
   <main className='h-full w-full'>
     <div className='flex flex-col gap-20'>
-      <Hero></Hero>
+      <Hero />
+      <Skills />
     </div>
   </main>
   )
