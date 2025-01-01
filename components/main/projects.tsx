414e8856e23467501898c49e3fd70d5914c04ae5
deep-patel21 <deepstudio21@gmail.com>
2025-01-01 12:53:05 -0500

    Change filepath to use media from an assets folder

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -16,43 +16,43 @@ const Projects = () => {
         className='text-[20px] z-[20] top-0 text-bold text-white mb-10 mt-[1px] text-center'>
             <i>Personal/Academic ventures in Software/Hardware</i>
         </motion.div>
-        <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
+        <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10 justify-around'>
            <ProjectCard 
-                src="/Notes4YouMainScreen.png"
+                src="/projects/Notes4YouMainScreen.png"
                 title="Notes4You Music App"
-                width={1000}
-                height={1000}
+                width={750}
+                height={750}
                 project_link = "https://docs.google.com/document/d/16hnnu0o__C1A-FUVzJwtuiF1q9_kjVvE3sSVBvgWtvM/edit?tab=t.0"
                 skills='Flask, Python, HTML, CSS, JS, OAuth'
                 description="Finds MIDI Piano tutorials for Spotify playlists. Leverages Spotify API and YouTubeAPI."/>
             <ProjectCard 
-                src="/LandingPage.png"
+                src="/projects/LandingPage.png"
                 title="Systematic Invesment Calculator"
-                width={1000}
-                height={1000}
+                width={750}
+                height={750}
                 project_link = "https://github.com/deep-patel21/Systematic-Investment-Planning"
                 skills='HTML, CSS, JS, Chart.js'
                 description="Highly customizable finance portfolio visualization tool. Allows year-by-year growth analysis."/>
             <ProjectCard 
-                src="/moviedb_react_version.png"
+                src="/projects/moviedb_react_version.png"
                 title="MovieDB"
-                width={1000}
-                height={1000}
+                width={750}
+                height={750}
                 project_link = "https://github.com/deep-patel21/MovieDB"
                 skills='SQL, UNIX, React.js, Oracle'
                 description="Database for all things film. Normalized. Visualized. Term project in CPS510."/>
             <ProjectCard 
                 src="/gpp.jpg"
                 title="General Purpose Processor"
-                width={1000}
-                height={1000}
+                width={750}
+                height={750}
                 project_link = "https://docs.google.com/document/d/1cN_pdQd3iZfBBKUJ1B91pH61KHAbWlCQ3FtCHFPSqdI/edit?tab=t.0"
                 skills='VHDL, Apache Quartus'
                 description="Combines decoders, FSMs, registers, and an ALU to create a GPP. Term project in COE328."/>
         </div>
         <div className='h-full py-[20px] w-full flex flex-col md:flex-row gap-10 px-10'>
             <ProjectCard 
-                src="/autoCl.png"
+                src="/projects/autoCl.png"
                 title="AutoCL Generator"
                 width={1000}
                 height={1000}
@@ -60,7 +60,7 @@ const Projects = () => {
                 skills='Python, docx2pdf, openpyxl, pathlib'
                 description="Input Excel file of companies, get folders with auto-made CV in .docx and .pdf"/>
             <ProjectCard 
-                src="/multisimproject.png"
+                src="/projects/multisimproject.png"
                 title="Multi-Stage BJT Amplifier"
                 width={1000}
                 height={1000}
@@ -68,7 +68,7 @@ const Projects = () => {
                 skills='National Instruments Multisim'
                 description="2-Stage amplifer to improve circuit capabilities under given contraints. Term project in ELE404."/>
             <ProjectCard 
-                src="/engineeringcalc1.png"
+                src="/projects/engineeringcalc1.png"
                 title="Engineering Economics Calculator"
                 width={1000}
                 height={1000}
@@ -86,7 +86,7 @@ const Projects = () => {
         </div>
         <div className='h-full py-[20px] w-full flex flex-col md:flex-row gap-10 px-10'>
             <ProjectCard 
-                src="/sorting.png"
+                src="/projects/sorting.png"
                 title="Sorting Algorithms Visualizer"
                 width={1000}
                 height={1000}
@@ -94,7 +94,7 @@ const Projects = () => {
                 skills='Python, Pygame, Algorithms'
                 description="Helps users visualize sorting algorithms. Use keyboard switches to cycle options."/>
             <ProjectCard 
-                src="/f1mouse.png"
+                src="/projects/f1mouse.png"
                 title="F1 Video Game Mouse Steering"
                 width={1000}
                 height={1000}
@@ -102,7 +102,7 @@ const Projects = () => {
                 skills='Python, vJoy, FreePIE'
                 description="Script to enable users without Xbox controllers to play F1 racing with PC mouse steering."/>
             <ProjectCard 
-                src="/bookstore1.png"
+                src="/projects/bookstore1.png"
                 title="Bookstore POS Application"
                 width={1000}
                 height={1000}
@@ -110,7 +110,7 @@ const Projects = () => {
                 skills='Java, JavaFX, Figma, Design Patterns'
                 description="POS application for owners to manage inventory and customers to buy books. Has login and serialization."/>    
             <ProjectCard 
-                src="/trafficlightsarduino.png"
+                src="/projects/trafficlightsarduino.png"
                 title="Automated Traffic Light Controller"
                 width={1000}
                 height={1000}
